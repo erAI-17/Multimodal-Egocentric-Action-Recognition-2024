b7d82fe744a7b07d1a63abc61451ea2b5a8faf3b
LeleFox <emanuele.raimondo@outlook.com>
2024-01-14 18:36:37 +0100

    Initial classifiers

--- models/FinalClassifier.py
+++ models/FinalClassifier.py
@@ -1,12 +1,64 @@
+import torch
 from torch import nn
+import torch.nn.functional as F
+from utils.args import args
+import utils
 
+input_size = 1024
+hidden_size = 512
 
-class Classifier(nn.Module):
-    def __init__(self, num_classes):
-        super().__init__()
-        """
-        [TODO]: the classifier should be implemented by the students and different variations of it can be tested
-        in order to understand which is the most performing one """
+#num_clips (5) * 1024
+#receives BATCH_SIZE (32) * 1024 
+
+class MLP(nn.Module):
+    def __init__(self):
+        num_classes, valid_labels, source_domain, target_domain = utils.utils.get_domains_and_labels(args)
+        ##???print("THIS IS NUM_CLASSES:",num_classes)
+        super(MLP, self).__init__()
+        self.fc1 = nn.Linear(input_size, hidden_size)
+        self.fc2 = nn.Linear(hidden_size, hidden_size)
+        self.classifier = nn.Linear(hidden_size, num_classes)
 
     def forward(self, x):
-        return self.classifier(x), {}
+        x = torch.relu(self.fc1(x))
+        x = torch.relu(self.fc2(x))
+        logits = self.classifier(x)
+        features = {"output features": x}  # Create a dictionary of features from last layer
+        return logits, features
+
+
+class LSTM(nn.Module):
+    def __init__(self, input_size, hidden_size, num_layers, output_size):
+        super(LSTM, self).__init__()
+        self.hidden_size = hidden_size
+        self.num_layers = num_layers
+        self.lstm = nn.LSTM(input_size, hidden_size, num_layers, batch_first=True)
+        self.fc = nn.Linear(hidden_size, output_size)
+
+    def forward(self, x):
+        h0 = torch.zeros(self.num_layers, x.size(0), self.hidden_size).to(x.device)
+        c0 = torch.zeros(self.num_layers, x.size(0), self.hidden_size).to(x.device)
+
+        out, _ = self.lstm(x, (h0, c0))
+        out = self.fc(out[:, -1, :])
+        return out
+   
+    
+class Transformer(nn.Module):
+    def __init__(self, input_size, hidden_size, num_layers, num_heads, dropout_rate):
+        super(Transformer, self).__init__()
+        self.embedding = nn.Embedding(input_size, hidden_size)
+        self.encoder = nn.TransformerEncoder(
+            nn.TransformerEncoderLayer(hidden_size, num_heads, dim_feedforward=hidden_size),
+            num_layers
+        )
+        self.fc = nn.Linear(hidden_size, input_size)
+        self.dropout = nn.Dropout(dropout_rate)
+
+    def forward(self, x):
+        embedded = self.embedding(x)
+        embedded = embedded.permute(1, 0, 2)  # (seq_len, batch_size, hidden_size)
+        encoded = self.encoder(embedded)
+        encoded = encoded.permute(1, 0, 2)  # (batch_size, seq_len, hidden_size)
+        logits = self.fc(self.dropout(encoded))
+        return F.log_softmax(logits, dim=2)
