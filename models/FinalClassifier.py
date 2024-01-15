9480be7bbaed059461f8e0631eeb298d171cf5c4
Emanuele <140118231+LeleFox@users.noreply.github.com>
2024-01-15 13:00:04 +0100

    Update FinalClassifier.py

--- models/FinalClassifier.py
+++ models/FinalClassifier.py
@@ -16,13 +16,11 @@ class MLP(nn.Module):
         super(MLP, self).__init__()
         self.fc1 = nn.Linear(input_size, hidden_size)
         self.fc2 = nn.Linear(hidden_size, hidden_size)
-        self.fc3 = nn.Linear(hidden_size, hidden_size)
         self.classifier = nn.Linear(hidden_size, num_classes)
 
     def forward(self, x):
         x = torch.relu(self.fc1(x))
-        x = torch.relu(self.fc2(x))
-        x = torch.relu(self.fc3(x))
+        #x = torch.relu(self.fc2(x))
         logits = self.classifier(x)
         features = {"output features": x}  # Create a dictionary of features from last layer
         return logits, features
