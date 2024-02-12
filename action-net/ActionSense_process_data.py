466475dc3f198ac56ae6086e3ff4e8b6d5a8ce8a
Emanuele Raimondo <140118231+LeleFox@users.noreply.github.com>
2024-02-12 15:28:11 +0100

    Update ActionSense_process_data.py

--- action-net/ActionSense_process_data.py
+++ action-net/ActionSense_process_data.py
@@ -23,7 +23,7 @@ device_streams_for_features = [
 subjects = ('S00_2.pkl', 'S01_1.pkl','S04_1.pkl', 'S08_1.pkl')
 
 def combine_pickle_files(output_file):
-        combined_df = pd.DataFrame() 
+        combined_df = pd.DataFrame()  # Initialize an empty DataFrame to store the merged data
 
         for file_name in os.listdir(EMG_data_path):
             if file_name.endswith('.pkl'):
@@ -40,10 +40,30 @@ def combine_pickle_files(output_file):
 
 
 def lowpass_filter(data, cutoff, Fs, order=5):
+    '''
+    Function that applies a low-pass filter to a given data set allowing you to remove high-frequency noise or unwanted frequency components from the data 
+    by filtering input data removing the frequencies above the cutoff frequency. 
+    Cutoff filters are useful for:
+        1) noise reduction: remove high-frequency components that may contain unwanted noise or interference
+        2) frequency isolation: isolate specific frequency bands of interest. For example, in audio engineering, a low-pass filter can be used to isolate the bass frequencies
+        3) Anti-aliasing
+        4) Smoothing: smooth out a signal by removing high-frequency fluctuations (unwanted variations) and highlight underlying trends
+
+    cutoff: maximum frequency allowed to pass through the filter.
+    Fs:sampling frequency of the input data (it is the number of samples taken per second) 
+    order (optional): the order of the filter. The default value is 5, which indicates a 5th-order filter. 
+    '''  
+
+    # Nyquist frequency represents the highest frequency that can be accurately represented in the sampled data (set to half of the sampling frequency).
     nyq = 0.5 * Fs 
+    #This normalization step ensures that the cutoff frequency is expressed as a fraction of the Nyquist frequency, which is a standard practice in signal processing. 
     normal_cutoff = cutoff / nyq 
+    
+    #butter  function from the  scipy.signal  module to design the filter coefficients (b,a) for the low-pass filter. 
+    #The 'butter' function takes the filter order, the normalized cutoff frequency, and the filter type as arguments. 
+    #In this case, the filter type is specified as 'low', indicating a low-pass filter, and the 
+    #analog  parameter is set to  False , indicating that we are designing a digital filter
     b, a = butter(order, normal_cutoff, btype='low', analog=False)
-<<<<<<< HEAD
     
     #filter coefficients (b,a) are then used to apply the low-pass filter to the input data. 
     #This is done using the  lfilter  function from the  scipy.signal  module. 
@@ -53,17 +73,16 @@ def lowpass_filter(data, cutoff, Fs, order=5):
     y = np.empty_like(data)
     for i,x in enumerate(data):
         y[i] = lfilter(b, a, x.T).T
-=======
-    y = lfilter(b, a, data.T).T
->>>>>>> d6a815366a3ba875c6f27d39b0e80983b6f693e9
     return y
 
 
 def split_train_test():
+    #read annotation files
     annotations_train = pd.read_pickle(os.path.join(annotations_path, 'ActionNet_train.pkl'))
     annotations_test = pd.read_pickle(os.path.join(annotations_path, 'ActionNet_test.pkl'))
-  
     emg_dataset = pd.read_pickle(os.path.join(EMG_data_path, 'combined_emg_dataset.pkl'))
+
+    #Inner join by indexes and file
     AN_train = annotations_train.merge(emg_dataset, on=['index','file'], how='inner')
     AN_test = annotations_test.merge(emg_dataset, on=['index', 'file'], how='inner')
     
@@ -71,8 +90,8 @@ def split_train_test():
 
 
 def Baseline(dataset):
+    #schema: ['index', 'file', 'description_x', 'labels', 'description_y', 'start','stop', 'myo_left_timestamps', 'myo_left_readings','myo_right_timestamps', 'myo_right_readings']
     new_rows = []
-<<<<<<< HEAD
     for subjectid in subjects: #SEPARATELY FOR EACH SUBJECT
         indices = np.where(dataset[:, 1] == subjectid)[0]
         data = dataset[indices] 
@@ -121,25 +140,6 @@ def Baseline(dataset):
     
     # Stack the augmented rows vertically with the original dataset
     dataset_with_baselines = np.vstack((dataset, reduced_new_rows))  
-=======
-    for i, row in enumerate(dataset): 
-        if i < dataset.shape[0]-1:
-            noActivity_start_ts = dataset[i+1][5]
-            noActivity_end_ts = dataset[i][6]
-            duration_s = (noActivity_start_ts - noActivity_end_ts )/6000
-            if duration_s < 5: #!5sec
-                continue
-
-            filtered_myo_left_ts = [ts for ts in row[7] if noActivity_start_ts <= ts < noActivity_end_ts]
-            filtered_myo_left_readings = [row[8][i] for i, ts in enumerate(row[7]) if np.any(((noActivity_start_ts <= ts) & (ts <= noActivity_end_ts)))]
-            
-            filtered_myo_right_ts = [ts for ts in row[9] if noActivity_start_ts <= ts < noActivity_end_ts]
-            filtered_myo_right_readings = [row[10][i] for i, ts in enumerate(row[9]) if np.any(((noActivity_start_ts <= ts) & (ts <= noActivity_end_ts)))]
-
-            new_rows.append([row[0], row[1], 'no_activity','baseline', 'no_activity', noActivity_end_ts, noActivity_start_ts, filtered_myo_left_ts, filtered_myo_left_readings, filtered_myo_right_ts, filtered_myo_right_readings])
-          
-    dataset_with_baselines = np.vstack((dataset, np.array(new_rows, dtype=object)))
->>>>>>> d6a815366a3ba875c6f27d39b0e80983b6f693e9
     
     return dataset_with_baselines
     
@@ -155,17 +155,9 @@ def Augmenting(dataset):
         if num_intervals[i] <= 1:    #if just 1 interval of 5 seconds, don't split
             continue
         
-<<<<<<< HEAD
         # Compute the start and stop timesteps for each interval of this row
         start_ts = row[5] + np.arange(num_intervals[i]) * 5
         stop_ts = start_ts + 5
-=======
-        start_ts = row[3] + np.arange(num_intervals[i]) * 5
-        stop_ts = start_ts + 5
-
-        filtered_myo_left_ts = [ts for ts in row[5] if start_ts <= ts < stop_ts]
-        filtered_myo_left_readings = [row[6][i] for i, ts in enumerate(row[5]) if np.any(((start_ts <= ts) & (ts <= stop_ts)))]
->>>>>>> d6a815366a3ba875c6f27d39b0e80983b6f693e9
         
         for j in range(len(start_ts)):
             filtered_myo_left_indices = np.where((start_ts[j] <= row[7]) & (row[7] < stop_ts[j]))[0]
@@ -179,7 +171,6 @@ def Augmenting(dataset):
             # Create new rows with the updated arrays
             new_rows.append([row[0], row[1], row[2], row[3], row[4], start_ts, stop_ts, filtered_myo_left_ts, filtered_myo_left_readings, filtered_myo_right_ts, filtered_myo_right_readings])
 
-<<<<<<< HEAD
     #!transform new_rows into a np array     
     new_rows = np.array(new_rows, dtype=object)
 
@@ -190,16 +181,6 @@ def Preprocessing(dataset):
     #schema: ['index', 'file', 'description_x', 'labels', 'description_y', 'start','stop', 'myo_left_timestamps', 'myo_left_readings','myo_right_timestamps', 'myo_right_readings']
     
     #Define filtering parameter.
-=======
-        new_rows.append(np.column_stack((start_ts, stop_ts, filtered_myo_left_ts, filtered_myo_left_readings, filtered_myo_right_ts, filtered_myo_right_readings)))
-
-    augmented_df = np.vstack((dataset, np.concatenate(new_rows)))
-  
-    return augmented_df
-
-
-def Preprocessing(dataset):
->>>>>>> d6a815366a3ba875c6f27d39b0e80983b6f693e9
     filter_cutoff_Hz = 5
     #absolute + filter + normalize SEPARATELY the myo-right and myo-left readings SEPARATELY for each subject      
     for subjectid in subjects: #SEPARATELY FOR EACH SUBJECT
@@ -232,7 +213,6 @@ def Preprocessing(dataset):
         myo_left_readings = lowpass_filter(myo_left_readings, filter_cutoff_Hz, Fs_left)
         myo_right_readings= lowpass_filter(myo_right_readings, filter_cutoff_Hz, Fs_right)
 
-<<<<<<< HEAD
         #normalize#!why /2 ??????
         reshaped_myo_left_readings = np.concatenate(myo_left_readings, axis=0)
         reshaped_myo_right_readings = np.concatenate(myo_left_readings, axis=0)
@@ -250,27 +230,12 @@ def Preprocessing(dataset):
         #*update original
         dataset[indices, 8] = myo_left_readings
         dataset[indices, 10] = myo_right_readings
-=======
-    t = dataset['time_s']
-    for myo_key in ('myo_left_readings', 'myo_right_readings'):
-        
-        Fs = (t.size - 1) / (t[-1] - t[0])      
-        
-        print(' Filtering %s with Fs %0.1f Hz to cutoff %f' % (myo_key, Fs, filter_cutoff_Hz))
-        y = np.abs(dataset[myo_key])
-        y = lowpass_filter(y, filter_cutoff_Hz, Fs)
-
-        print(' Normalizing %s with min/max [%0.1f, %0.1f]' % (myo_key, np.amin(y), np.amax(y)))
-        y = y / ((np.amax(y) - np.amin(y))/2) 
-        y = y - np.amin(y) - 1
->>>>>>> d6a815366a3ba875c6f27d39b0e80983b6f693e9
         
     return dataset
 
 
 if __name__ == '__main__':
     
-<<<<<<< HEAD
     ##Combine all pkl data files from different subjects into one
     #combine_pickle_files(os.path.join(EMG_data_path ,'combined_emg_dataset.pkl'))
     
@@ -279,11 +244,6 @@ if __name__ == '__main__':
     # print(df.shape)
     # print(df.head())
     
-=======
-    #Combine all pkl data files from different subjects into one
-    # combine_pickle_files(os.path.join(EMG_data_path ,'combined_emg_dataset.pkl'))
-  
->>>>>>> d6a815366a3ba875c6f27d39b0e80983b6f693e9
     #Load all EMG data, merge it with annotations 
     #split dataset into train and test splits according to annotations files
     # AN_train, AN_test = split_train_test() #returns pd dataframe
@@ -292,7 +252,6 @@ if __name__ == '__main__':
     # print(AN_train.shape)
     # print(AN_train.head())
     
-<<<<<<< HEAD
     # ##Convert the datasets to NumPy arrays for better efficiency
     # ##schema: ['index', 'file', 'description_x', 'labels', 'description_y', 'start','stop', 'myo_left_timestamps', 'myo_left_readings','myo_right_timestamps', 'myo_right_readings']
     # AN_train = AN_train.copy().to_numpy() #[527*11]
@@ -324,25 +283,10 @@ if __name__ == '__main__':
     # print(AN_train_aug_DF.shape)
     # print(AN_train_aug_DF.head())
     
-=======
-    # Convert the datasets to NumPy arrays for better efficiency
-    AN_train = AN_train.copy().to_numpy() 
-    AN_test = AN_test.copy().to_numpy()
-    
-    #Introduce baselines actions
-    AN_train_base = Baseline(AN_train) #[735*11] 
-    AN_test_base = Baseline(AN_test)   #[*11]
-     
-    #Augment actions into smaller actions of 5seconds each
-    # AN_train_aug = Augmenting(AN_train_base) 
-    # AN_test_aug = Augmenting(AN_test_base) 
-
->>>>>>> d6a815366a3ba875c6f27d39b0e80983b6f693e9
     # #Filter, Normalize and Augment
     # AN_train_aug_preprocessed = Preprocessing(AN_train) #AN_train_base #AN_train_aug
     # AN_test_aug_preprocessed = Preprocessing(AN_test) #AN_test_base #AN_test_aug
     
-<<<<<<< HEAD
     # #!TRY PRINT
     # AN_train_aug_preprocessed = pd.DataFrame(AN_train_aug_preprocessed, columns=['index', 'file', 'description_x', 'labels', 'description_y', 'start','stop', 'myo_left_timestamps', 'myo_left_readings','myo_right_timestamps', 'myo_right_readings'])
     # print()
@@ -355,9 +299,6 @@ if __name__ == '__main__':
     # AN_train_final = pd.DataFrame(AN_train_aug_preprocessed, columns=['index', 'file', 'description_x', 'labels', 'description_y', 'start','stop', 'myo_left_timestamps', 'myo_left_readings','myo_right_timestamps', 'myo_right_readings'])
     # AN_test_final = pd.DataFrame(AN_train_aug_preprocessed, columns=['index', 'file', 'description_x', 'labels', 'description_y', 'start','stop', 'myo_left_timestamps', 'myo_left_readings','myo_right_timestamps', 'myo_right_readings'])
     
-=======
-    #Convert back to pd dataframes
->>>>>>> d6a815366a3ba875c6f27d39b0e80983b6f693e9
     # #Save preprocessed dataset
     # filepath = EMG_data_path + 'preprocessed_data_train.pkl'
     # with open(filepath, 'wb') as pickle_file:
@@ -365,14 +306,9 @@ if __name__ == '__main__':
     
     # filepath = EMG_data_path + + 'preprocessed_data_test.pkl'
     # with open(filepath, 'wb') as pickle_file:
-<<<<<<< HEAD
     #     pickle.dump(AN_test_final, pickle_file, protocol=pickle.HIGHEST_PROTOCOL)
     
     df = pd.read_pickle('./Action-Net/data/EMG_datapreprocessed_data_train.pkl')
     print(df.columns)
     print(df.shape)
     print(df.head(20))
-=======
-    #     pickle.dump(df, pickle_file, protocol=pickle.HIGHEST_PROTOCOL)
-    
->>>>>>> d6a815366a3ba875c6f27d39b0e80983b6f693e9
