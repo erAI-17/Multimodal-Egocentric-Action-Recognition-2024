677b97ac2e2a8323fc8323873411c1678bce2071
kangchenghua <chenghuakang@gmail.com>
2024-02-12 15:45:13 +0100

    fix

--- save_feat.py
+++ save_feat.py
@@ -11,6 +11,8 @@ import numpy as np
 import os
 import models as model_list
 import tasks
+import torch.nn as nn
+import torch.nn.functional as functional
 
 # global variables among training functions
 modalities = None
@@ -155,6 +157,17 @@ def save_feat(model, loader, device, it, num_classes):
         f.write("[%d/%d]\tAcc@top1: %.2f%%\n" % (it, args.train.num_iter, test_results['top1']))
 
     return test_results
+
+class MLP(nn.Module):
+    def __init__(self, input_size, hidden_size, output_size):
+        super(MLP, self).__init__()
+        self.fc1 = nn.Linear(input_size, hidden_size)
+        self.fc2 = nn.Linear(hidden_size, output_size)
+
+    def forward(self, x):
+        x = functional.relu(self.fc1(x))
+        x = self.fc2(x)
+        return x
 
 
 if __name__ == '__main__':
