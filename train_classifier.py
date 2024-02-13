0ca19bae38d7711ff176b6689b596aeeacc2853f
LeleFox <emanuele.raimondo@outlook.com>
2024-02-13 16:45:37 +0100

    update validation function

--- train_classifier.py
+++ train_classifier.py
@@ -48,7 +48,7 @@ def main():
     num_classes, valid_labels, source_domain, target_domain = utils.utils.get_domains_and_labels(args)
     # device where everything is run
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-    print('DEVICE USED IS:', device)
+
     # these dictionaries are for more multi-modal training/testing, each key is a modality used
     models = {}
     logger.info("Instantiating models per modality")
@@ -68,10 +68,11 @@ def main():
         # resume_from argument is adopted in case of restoring from a checkpoint
         if args.resume_from is not None:
             action_classifier.load_last_model(args.resume_from)
-        
+        # define number of iterations I'll do with the actual batch: we do not reason with epochs but with iterations
+        # i.e. number of batches passed
+        # notice, here it is multiplied by tot_batch/batch_size since gradient accumulation technique is adopted
         training_iterations = args.train.num_iter * (args.total_batch // args.batch_size)
-        
-        #*All dataloaders are generated here
+        # all dataloaders are generated here
         train_loader = torch.utils.data.DataLoader(EpicKitchensDataset(args.dataset.shift.split("-")[0], modalities,
                                                                        'train', args.dataset, None, None, None,
                                                                        None, load_feat=True),
@@ -83,7 +84,6 @@ def main():
                                                                      None, load_feat=True),
                                                  batch_size=args.batch_size, shuffle=False,
                                                  num_workers=args.dataset.workers, pin_memory=True, drop_last=False)
-        
         train(action_classifier, train_loader, val_loader, device, num_classes)
 
     elif args.action == "validate":
@@ -100,8 +100,8 @@ def main():
 
 def train(action_classifier, train_loader, val_loader, device, num_classes):
     """
-    function to train 1 model for modality on the training set
-    action_classifier: Task containing the model to be trained for each modality
+    function to train the model on the test set
+    action_classifier: Task containing the model to be trained
     train_loader: dataloader containing the training data
     val_loader: dataloader containing the validation data
     device: device on which you want to test
@@ -112,24 +112,26 @@ def train(action_classifier, train_loader, val_loader, device, num_classes):
     data_loader_source = iter(train_loader)
     action_classifier.train(True)
     action_classifier.zero_grad()
-    
     iteration = action_classifier.current_iter * (args.total_batch // args.batch_size)
 
+    # the batch size should be total_batch but batch accumulation is done with batch size = batch_size.
+    # real_iter is the number of iterations if the batch size was really total_batch
     for i in range(iteration, training_iterations):
+        # iteration w.r.t. the paper (w.r.t the bs to simulate).... i is the iteration with the actual bs( < tot_bs)
         real_iter = (i + 1) / (args.total_batch // args.batch_size)
         if real_iter == args.train.lr_steps:
             # learning rate decay at iteration = lr_steps
             action_classifier.reduce_learning_rate()
-        # gradient_accumulation_step is a bool used to understand if we accumulated at least total_batch samples' gradient
+        # gradient_accumulation_step is a bool used to understand if we accumulated at least total_batch
+        # samples' gradient
         gradient_accumulation_step = real_iter.is_integer()
 
         """
         Retrieve the data from the loaders
         """
         start_t = datetime.now()
-        
-        #* the following code is necessary as we do not reason in terms of epochs so,
-        #* as soon as the dataloader is finished we need to redefine the iterator
+        # the following code is necessary as we do not reason in epochs so as soon as the dataloader is finished we need
+        # to redefine the iterator
         try:
             source_data, source_label = next(data_loader_source)
         except StopIteration:
@@ -139,17 +141,20 @@ def train(action_classifier, train_loader, val_loader, device, num_classes):
 
         logger.info(f"Iteration {i}/{training_iterations} batch retrieved! Elapsed time = "
                     f"{(end_t - start_t).total_seconds() // 60} m {(end_t - start_t).total_seconds() % 60} s")
-        
-        
+
+        ''' Action recognition'''
         source_label = source_label.to(device)
         data = {}
-        
-        for m in modalities:
-            data[m] = source_data[m].to(device)
-        logits, _ = action_classifier.forward(data)
-        action_classifier.compute_loss(logits, source_label, loss_weight=1)
-        action_classifier.backward(retain_graph=False)
-        action_classifier.compute_accuracy(logits, source_label)
+
+        for clip in range(args.train.num_clips):
+            # in case of multi-clip training one clip per time is processed
+            for m in modalities:
+                data[m] = source_data[m][:, clip].to(device)
+
+            logits, _ = action_classifier.forward(data)
+            action_classifier.compute_loss(logits, source_label, loss_weight=1)
+            action_classifier.backward(retain_graph=False)
+            action_classifier.compute_accuracy(logits, source_label)
 
         # update weights and zero gradients if total_batch samples are passed
         if gradient_accumulation_step:
@@ -203,14 +208,14 @@ def validate(model, val_loader, device, it, num_classes):
                 logits[m] = torch.zeros((args.test.num_clips, batch, num_classes)).to(device)
 
             clip = {}
-            for i_c in range(args.test.num_clips):
-                for m in modalities:
-                    clip[m] = data[m][:, i_c].to(device)
+            
+            for m in modalities:
+                clip[m] = data[m].to(device)
 
-                output, _ = model(clip)
-                for m in modalities:
-                    logits[m][i_c] = output[m]
-
+            output, _ = model(clip)
+            for m in modalities:
+                logits[m] = output[m]
+            
             for m in modalities:
                 logits[m] = torch.mean(logits[m], dim=0)
 
