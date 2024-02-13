f8de4c89f0a2dfa8111adae5df45576d70ba8e1f
LeleFox <emanuele.raimondo@outlook.com>
2024-02-13 13:34:52 +0100

    update models and feat_avg

--- models/FinalClassifier.py
+++ models/FinalClassifier.py
@@ -20,7 +20,6 @@ class MLP(nn.Module):
         self.avg_pool = nn.AdaptiveAvgPool1d(1) 
         
     def forward(self, x):
-        
         if args.feat_avg:   #*Feature Averaging
             x = self.avg_pool(x.permute(0, 2, 1))  
             x = x.permute(0, 2, 1)
@@ -43,17 +42,17 @@ class LSTM(nn.Module):
         num_classes, valid_labels, source_domain, target_domain = utils.utils.get_domains_and_labels(args)
         super(LSTM, self).__init__()
         self.lstm = nn.LSTM(input_size, hidden_size, num_layers, batch_first=True)
-        self.fc = nn.Linear(hidden_size, num_classes)
+        self.dropout = nn.Dropout(args.models.RGB.dropout)
+        self.relu = nn.ReLU()
+        self.classifier = nn.Linear(hidden_size, num_classes)
 
     def forward(self, x):
-        h0 = torch.zeros(self.num_layers, x.size(0), self.hidden_size).to(x.device)
-        c0 = torch.zeros(self.num_layers, x.size(0), self.hidden_size).to(x.device)
-
-        out, _ = self.lstm(x, (h0, c0))
-        out = self.fc(out[:, -1, :])
+        out, _ = self.lstm(x)
+        out = self.dropout(out)
+        out = self.relu(out)
+        out = self.fc(out[:, -1, :]) # extract last output of the sequence (the one obtained after all the timesteps)
         return out, {}
    
-    
 class Transformer(nn.Module):
     def __init__(self, input_size, hidden_size, num_layers, num_heads, dropout_rate):
         super(Transformer, self).__init__()
@@ -72,3 +71,54 @@ class Transformer(nn.Module):
         encoded = encoded.permute(1, 0, 2)  # (batch_size, seq_len, hidden_size)
         logits = self.fc(self.dropout(encoded))
         return F.log_softmax(logits, dim=2)
+
+###############
+#ACTION-NET MODELS
+##############
+
+class MLP_EMG(nn.Module):
+    def __init__(self):
+        num_classes, valid_labels, source_domain, target_domain = utils.utils.get_domains_and_labels(args)
+        super(MLP, self).__init__()
+        self.fc1 = nn.Linear(input_size, hidden_size)
+        self.dropout1 = nn.Dropout(args.models.RGB.dropout)
+        self.fc2 = nn.Linear(hidden_size, hidden_size)
+        self.dropout2 = nn.Dropout(args.models.RGB.dropout)
+        self.classifier = nn.Linear(hidden_size, num_classes)
+        
+        self.avg_pool = nn.AdaptiveAvgPool1d(1) 
+        
+    def forward(self, x):
+        if args.feat_avg:   #*Feature Averaging
+            x = self.avg_pool(x.permute(0, 2, 1))  
+            x = x.permute(0, 2, 1)
+            
+        x = torch.relu(self.fc1(x))
+        x = torch.dropout1(x)
+        x = torch.relu(self.fc2(x))
+        x = torch.dropout2(x)
+        logits = self.classifier(x)
+        
+        if args.feat_avg==False:   #*Logits Averaging
+            logits = self.avg_pool(logits.permute(0, 2, 1)) 
+            logits = logits.permute(0, 2, 1)
+
+        features = {"output features": x}  # Create a dictionary of features from last layer
+        return logits, features
+
+class LSTM_EMG(nn.Module):
+    def __init__(self, num_layers=1):
+        num_classes, valid_labels, source_domain, target_domain = utils.utils.get_domains_and_labels(args)
+        super(LSTM, self).__init__()
+        self.lstm = nn.LSTM(input_size, hidden_size, num_layers, batch_first=True)
+        self.dropout = nn.Dropout(args.models.RGB.dropout)
+        self.relu = nn.ReLU()
+        self.classifier = nn.Linear(hidden_size, num_classes)
+
+    def forward(self, x):
+        out, _ = self.lstm(x)
+        out = self.dropout(out)
+        out = self.relu(out)
+        out = self.fc(out[:, -1, :]) # extract last output of the sequence (the one obtained after all the timesteps)
+        return out, {}
+   
