4c170983d1fbb4728fc26748f5ddddb311a897b8
LeleFox <emanuele.raimondo@outlook.com>
2024-02-16 15:34:17 +0100

    run

--- models/__init__.py
+++ models/__init__.py
@@ -1,3 +1,3 @@
 from models.VideoModel import VideoModel
 from models.I3D import I3D
-from models.FinalClassifier import MLP, LSTM, Transformer, TRN, LSTM_EMG
+from models.FinalClassifier import MLP, LSTM, Transformer, LSTM_EMG #TRN
