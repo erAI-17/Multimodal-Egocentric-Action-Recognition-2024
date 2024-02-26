15fe6210b7b2d03dfa262b5a89736576ce94a6b1
LeleFox <emanuele.raimondo@outlook.com>
2024-02-26 12:34:12 +0100

    update

--- action-net/Action-Net_process_data.py
+++ action-net/Action-Net_process_data.py
@@ -150,8 +150,8 @@ def Augmenting(data):
         start_ts = row[5] + np.arange(num_intervals[i]) * 5
         stop_ts = start_ts + 5
 
-        mean_length = 1000
-        for j in range(num_intervals[i]-1):
+        mean_length = 900
+        for j in range(num_intervals[i]):
             filtered_myo_left_indices = np.where((start_ts[j] <= row[7]) & (row[7] < stop_ts[j]))[0] #(801,)
             
             if filtered_myo_left_indices.shape[0] > mean_length:
