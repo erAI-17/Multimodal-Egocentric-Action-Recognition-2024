70e00ec52d80c70b830db15e0cb356b7879c567b
LeleFox <emanuele.raimondo@outlook.com>
2024-02-28 21:25:30 +0100

    update network

--- models/FinalClassifier.py
+++ models/FinalClassifier.py
@@ -163,3 +163,45 @@ class LSTM_EMG(nn.Module):
         out = self.relu(out)
         out = self.fc(out[:, -1, :]) # extract last output of the sequence (the one obtained after all the timesteps)
         return out, {}
+    
+    
+class MLP_EMG(nn.Module):
+    def __init__(self):
+        input_size = 16
+        hidden_size = 512
+        num_classes, valid_labels, source_domain, target_domain = utils.utils.get_domains_and_labels(args)
+        super(MLP, self).__init__()
+        self.fc1 = nn.Linear(input_size, hidden_size)
+        self.fc2 = nn.Linear(hidden_size, hidden_size)
+        self.fc3 = nn.Linear(hidden_size, num_classes)
+        
+        self.dropout= nn.Dropout(args.models.RGB.dropout)
+        self.relu = nn.ReLU()
+        self.avg_pool = nn.AdaptiveAvgPool1d(1) 
+        
+    def forward(self, x):
+        x = x.float()
+        if args.feat_avg:   #*Feature Averaging
+            x = self.avg_pool(x.permute(0, 2, 1))  
+            x = x.permute(0, 2, 1)
+            x = x.squeeze(dim=1)
+            x = self.fc1(x)
+            x = self.dropout(x)
+            x = self.relu(x)
+            x = self.fc2(x)
+            x = self.dropout(x)
+            x = self.relu(x)
+            logits = self.fc3((x))
+        else:              #*Logits Averaging
+            x = self.fc1(x)
+            x = self.dropout(x)
+            x = self.relu(x)
+            x = self.fc2(x)
+            x = self.dropout(x)
+            x = self.relu(x)
+            logits = self.fc3(x)
+            logits = self.avg_pool(logits.permute(0, 2, 1)) 
+            logits = logits.permute(0, 2, 1)
+            logits = logits.squeeze(dim=1)
+       
+        return logits, {}
